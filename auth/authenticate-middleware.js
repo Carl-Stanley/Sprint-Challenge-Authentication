7539b5148ff1e096e6313e554424e537779200fe
Carl-Stanley <cds.beml@gmail.com>
2019-12-14 11:14:42 -0700

    Commit have server running. Still testing

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,18 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token,'aeaeiouAndSometimesY', (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ you: 'Access Denied!' }); 
+      } else {
+        req.user = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Token Missing!" });
+  }
+  
 };
