39e406e8760315910178c94b0c68c821728dc795
Carl-Stanley <cds.beml@gmail.com>
2019-12-15 12:40:40 -0700

    Commit

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,31 @@
+const server = require("../api/server.js");
+
+const request = require("supertest");
+
+const db = require("../database/dbConfig.js");
+
+describe("POST /register", () => {
+  it("will return JSON", () => {
+    return request(server)
+      .post("/api/auth/register")
+      .send({
+        username: "carl25",
+        password: "mephistaclees"
+      })
+      .then(res => {
+        expect(res.type).toMatch(/json/);
+      });
+  });
+
+  it("will return 200", () => {
+    return request(server)
+      .post("/api/auth/login")
+      .send({
+        username: "carl25",
+        password: "mephistaclees"
+      })
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
